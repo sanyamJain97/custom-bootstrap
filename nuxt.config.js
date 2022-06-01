4449f9822989b89e22d31c928b2b107aa04dd540
sanyamJain97 <sanyamj.jaipur@gmail.com>
2022-06-01 12:59:22 +0530

    First Commit

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+// function myRandom() {
+//   var temp = Math.floor(Math.random() * 5);
+//   console.log('sanyam', temp);
+//   return temp;
+// }
+const env = require('dotenv').config()
+
+console.log(Math.floor(Math.random() * 5));
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  env: env.parsed,
+  head: {
+    title: 'test',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    Math.floor(Math.random() * 5) > 1 ? '~/assets/scss/custom-green.scss' : '~/assets/scss/custom-red.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins/bootstrap.js'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    'bootstrap-vue/nuxt'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="design-form">
+<div class="p-3 mb-5 bg-primary text-white">.bg-primary</div>
+<span>Progress Bar </span>
+<div class="progress mb-5">
+  <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+  <form class="mb-5">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+  <div class="jumbotron bg-primary">
+    <h1 class="display-4">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <a class="btn btn-secondary btn-lg" href="#" role="button">Learn more</a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+<style lang="scss">
+.design-form {
+ position: absolute;
+ top: 50%;
+ left: 50%;
+ transform: translate(-50%,-50%);
+}
+// @import '~assets/scss/custom.scss';
+// h1 {
+//   color: $primary;
+// }
+
+</style>
